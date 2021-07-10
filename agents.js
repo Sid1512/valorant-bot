f72669fb91f5c309ab4a6edfdbd347cfbaa4c282
Siddharth Barjatya <189201053.siddharth@gmail.com>
2021-07-10 23:44:25 +0530

    First Commit

---
+++ agents.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+const { MessageEmbed } = require('discord.js')
+
+let agents = fs.readFileSync('agents.json')
+agents = JSON.parse(agents)
+
+function info (message, number) {
+  const agentEmbed = new MessageEmbed()
+    .setColor('#ffffff')
+    .setAuthor('Valorant Bot', 'https://static.wikia.nocookie.net/valorant_esports_gamepedia_en/images/d/d9/Logo_square.png/revision/latest?cb=20200404214157')
+    .setTitle(agents.agent[number].name)
+    .setThumbnail(agents.agent[number].avatar)
+    .setDescription(agents.agent[number].bio)
+    .addField('\u200B\nRole', agents.agent[number].role, true)
+    .addField('\u200B\nOrigin', agents.agent[number].origin, true)
+    .addField('\u200B\nGender', agents.agent[number].gender, true)
+    .addField('API_Name', agents.agent[number].api_name, true)
+    .addField('\u200B\n**Abilities**', '** **')
+    .addField(agents.agent[number].abilities[0].displayName, agents.agent[number].abilities[0].description)
+    .addField(agents.agent[number].abilities[1].displayName, agents.agent[number].abilities[1].description)
+    .addField(agents.agent[number].abilities[2].displayName, agents.agent[number].abilities[2].description)
+    .addField(agents.agent[number].abilities[3].displayName, agents.agent[number].abilities[3].description)
+  // const agentPhotoEmbed = new MessageEmbed()
+  //     .setColor('#ffffff')
+  //     .setImage(agents.agent[number].fullPortrait);
+  message.channel.send(agentEmbed)
+  // message.channel.send(agentPhotoEmbed);
+}
+
+module.exports = { info }
---
+++ test.js
@@ -0,0 +1,18 @@
+https://www.geeksforgeeks.org/how-to-make-javascript-wait-for-a-api-request-to-return/
+const rankemojils = ['<:ironup:861307057734156329>', '<:irondown:861307058372739083>', '<:bronzeup:861307057842683954>', '<:bronzedown:861307057743724544>', '<:silverup:861307057760239626>', '<:silverdown:861307058338529280>', '<:goldup:861307059169394728>', '<:golddown:861307058045321306>', '<:platinumup:861307058368282645>', '<:platinumdown:861307057969561630>', '<:diamondup:861307057725243443>', '<:diamonddown:861307057695883264>', '<:immortalup:861307057507532811>', '<:immortaldown:861307057411981333>', '<:radiantup:861307057780031519>', '<:radiantdown:861307058045190144>'];
+Astra
+Breach
+Brimstone
+Cypher
+Jett
+KAYO
+Killjoy
+Omen
+Phoenix
+Raze
+Reyna
+Sage
+Skye
+Sova
+Viper
+Yoru
