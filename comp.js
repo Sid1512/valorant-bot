e4cba009fd03c8a0b193f1ce4f3bcf1c6d95c476
Siddharth Barjatya <189201053.siddharth@gmail.com>
2021-07-19 11:55:19 +0530

    Update comp.js

--- comp.js
+++ comp.js
@@ -12,7 +12,7 @@ tiers = JSON.parse(tiers)
 const region = ['ap', 'eu', 'na', 'kr']
 
 async function comp (message, username, user, tag) {
-  const teamwin = [['Red', ''], ['Blue', '']]
+  const teamwin = ['', '']
   let check = -1
   for (const r of region) {
     await fetch('http://api.henrikdev.xyz/valorant/v3/matches/' + r + '/' + user + '/' + tag).then(res => {
@@ -25,10 +25,10 @@ async function comp (message, username, user, tag) {
               let playerAgentTeam
               if (game.teams.red.has_won) {
                 setColor = '#ff0000'
-                teamwin[0][1] = ' (Winner)'
+                teamwin[0] = ' (Winner)'
               } else {
                 setColor = '#0000ff'
-                teamwin[1][1] = ' (Winner)'
+                teamwin[1] = ' (Winner)'
               }
               for (const player of game.players.all_players) {
                 if ((player.name + player.tag).toLowerCase() === (user + tag).toLowerCase()) {
@@ -36,26 +36,26 @@ async function comp (message, username, user, tag) {
                   break
                 }
               }
-              const red = `${(game.players.red[0].character) + ' ' + tiers.tier[game.players.red[0].currenttier].rankIcon}
-              ${game.players.red[1].character + ' ' + tiers.tier[game.players.red[1].currenttier].rankIcon}
-              ${game.players.red[2].character + ' ' + tiers.tier[game.players.red[2].currenttier].rankIcon}
-              ${game.players.red[3].character + ' ' + tiers.tier[game.players.red[3].currenttier].rankIcon}
-              ${game.players.red[4].character + ' ' + tiers.tier[game.players.red[4].currenttier].rankIcon}`
-              const blue = `${(game.players.blue[0].character) + ' ' + tiers.tier[game.players.blue[0].currenttier].rankIcon}
-              ${game.players.blue[1].character + ' ' + tiers.tier[game.players.blue[1].currenttier].rankIcon}
-              ${game.players.blue[2].character + ' ' + tiers.tier[game.players.blue[2].currenttier].rankIcon}
-              ${game.players.blue[3].character + ' ' + tiers.tier[game.players.blue[3].currenttier].rankIcon}
-              ${game.players.blue[4].character + ' ' + tiers.tier[game.players.blue[4].currenttier].rankIcon}`
+              const red = `${tiers.tier[game.players.red[0].currenttier].rankIcon + ' ' + game.players.red[0].name + '#' + game.players.red[0].tag + ' **[' + game.players.red[0].character + ']**'}
+              ${tiers.tier[game.players.red[1].currenttier].rankIcon + ' ' + game.players.red[1].name + '#' + game.players.red[1].tag + ' **[' + game.players.red[1].character + ']**'}
+              ${tiers.tier[game.players.red[2].currenttier].rankIcon + ' ' + game.players.red[2].name + '#' + game.players.red[2].tag + ' **[' + game.players.red[2].character + ']**'}
+              ${tiers.tier[game.players.red[3].currenttier].rankIcon + ' ' + game.players.red[3].name + '#' + game.players.red[3].tag + ' **[' + game.players.red[3].character + ']**'}
+              ${tiers.tier[game.players.red[4].currenttier].rankIcon + ' ' + game.players.red[4].name + '#' + game.players.red[4].tag + ' **[' + game.players.red[4].character + ']**'}`
+              const blue = `${tiers.tier[game.players.blue[0].currenttier].rankIcon + ' ' + game.players.blue[0].name + '#' + game.players.blue[0].tag + ' **[' + game.players.blue[0].character + ']**'}
+              ${tiers.tier[game.players.blue[1].currenttier].rankIcon + ' ' + game.players.blue[1].name + '#' + game.players.blue[1].tag + ' **[' + game.players.blue[1].character + ']**'}
+              ${tiers.tier[game.players.blue[2].currenttier].rankIcon + ' ' + game.players.blue[2].name + '#' + game.players.blue[2].tag + ' **[' + game.players.blue[2].character + ']**'}
+              ${tiers.tier[game.players.blue[3].currenttier].rankIcon + ' ' + game.players.blue[3].name + '#' + game.players.blue[3].tag + ' **[' + game.players.blue[3].character + ']**'}
+              ${tiers.tier[game.players.blue[4].currenttier].rankIcon + ' ' + game.players.blue[4].name + '#' + game.players.blue[4].tag + ' **[' + game.players.blue[4].character + ']**'}`
               const sb =
-              `╔══════════════════╦════════╦══════════════════╦════════╗
-║${('Red Team' + teamwin[0][1]).padEnd(18)}║${('K/D/A').padEnd(8)}║${('Blue Team' + teamwin[1][1]).padEnd(18)}║${('K/D/A').padEnd(8)}║
-╠══════════════════╬════════╬══════════════════╬════════╣
-║${(game.players.red[0].name + '#' + game.players.red[0].tag).padEnd(18)}║${(game.players.red[0].stats.kills + '/' + game.players.red[0].stats.deaths + '/' + game.players.red[0].stats.assists).padEnd(8)}║${(game.players.blue[0].name + '#' + game.players.blue[0].tag).padEnd(18)}║${(game.players.blue[0].stats.kills + '/' + game.players.blue[0].stats.deaths + '/' + game.players.blue[0].stats.assists).padEnd(8)}║
-║${(game.players.red[1].name + '#' + game.players.red[1].tag).padEnd(18)}║${(game.players.red[1].stats.kills + '/' + game.players.red[1].stats.deaths + '/' + game.players.red[1].stats.assists).padEnd(8)}║${(game.players.blue[1].name + '#' + game.players.blue[1].tag).padEnd(18)}║${(game.players.blue[1].stats.kills + '/' + game.players.blue[1].stats.deaths + '/' + game.players.blue[1].stats.assists).padEnd(8)}║
-║${(game.players.red[2].name + '#' + game.players.red[2].tag).padEnd(18)}║${(game.players.red[2].stats.kills + '/' + game.players.red[2].stats.deaths + '/' + game.players.red[2].stats.assists).padEnd(8)}║${(game.players.blue[2].name + '#' + game.players.blue[2].tag).padEnd(18)}║${(game.players.blue[2].stats.kills + '/' + game.players.blue[2].stats.deaths + '/' + game.players.blue[2].stats.assists).padEnd(8)}║
-║${(game.players.red[3].name + '#' + game.players.red[3].tag).padEnd(18)}║${(game.players.red[3].stats.kills + '/' + game.players.red[3].stats.deaths + '/' + game.players.red[3].stats.assists).padEnd(8)}║${(game.players.blue[3].name + '#' + game.players.blue[3].tag).padEnd(18)}║${(game.players.blue[3].stats.kills + '/' + game.players.blue[3].stats.deaths + '/' + game.players.blue[3].stats.assists).padEnd(8)}║
-║${(game.players.red[4].name + '#' + game.players.red[4].tag).padEnd(18)}║${(game.players.red[4].stats.kills + '/' + game.players.red[4].stats.deaths + '/' + game.players.red[4].stats.assists).padEnd(8)}║${(game.players.blue[4].name + '#' + game.players.blue[4].tag).padEnd(18)}║${(game.players.blue[4].stats.kills + '/' + game.players.blue[4].stats.deaths + '/' + game.players.blue[4].stats.assists).padEnd(8)}║
-╚══════════════════╩════════╩══════════════════╩════════╝`
+              `╔═══════════╦══════════╦═══════════╦══════════╗
+║ ${('Red Team').padEnd(9)} ║ ${('K/D/A').padEnd(8)} ║ ${('Blue Team').padEnd(9)} ║ ${('K/D/A').padEnd(8)} ║
+╠═══════════╬══════════╬═══════════╬══════════╣
+║ ${(game.players.red[0].character).padEnd(9)} ║ ${(game.players.red[0].stats.kills + '/' + game.players.red[0].stats.deaths + '/' + game.players.red[0].stats.assists).padEnd(8)} ║ ${(game.players.blue[0].character).padEnd(9)} ║ ${(game.players.blue[0].stats.kills + '/' + game.players.blue[0].stats.deaths + '/' + game.players.blue[0].stats.assists).padEnd(8)} ║
+║ ${(game.players.red[1].character).padEnd(9)} ║ ${(game.players.red[1].stats.kills + '/' + game.players.red[1].stats.deaths + '/' + game.players.red[1].stats.assists).padEnd(8)} ║ ${(game.players.blue[1].character).padEnd(9)} ║ ${(game.players.blue[1].stats.kills + '/' + game.players.blue[1].stats.deaths + '/' + game.players.blue[1].stats.assists).padEnd(8)} ║
+║ ${(game.players.red[2].character).padEnd(9)} ║ ${(game.players.red[2].stats.kills + '/' + game.players.red[2].stats.deaths + '/' + game.players.red[2].stats.assists).padEnd(8)} ║ ${(game.players.blue[2].character).padEnd(9)} ║ ${(game.players.blue[2].stats.kills + '/' + game.players.blue[2].stats.deaths + '/' + game.players.blue[2].stats.assists).padEnd(8)} ║
+║ ${(game.players.red[3].character).padEnd(9)} ║ ${(game.players.red[3].stats.kills + '/' + game.players.red[3].stats.deaths + '/' + game.players.red[3].stats.assists).padEnd(8)} ║ ${(game.players.blue[3].character).padEnd(9)} ║ ${(game.players.blue[3].stats.kills + '/' + game.players.blue[3].stats.deaths + '/' + game.players.blue[3].stats.assists).padEnd(8)} ║
+║ ${(game.players.red[4].character).padEnd(9)} ║ ${(game.players.red[4].stats.kills + '/' + game.players.red[4].stats.deaths + '/' + game.players.red[4].stats.assists).padEnd(8)} ║ ${(game.players.blue[4].character).padEnd(9)} ║ ${(game.players.blue[4].stats.kills + '/' + game.players.blue[4].stats.deaths + '/' + game.players.blue[4].stats.assists).padEnd(8)} ║
+╚═══════════╩══════════╩═══════════╩══════════╝`
               // message.channel.send(rr)
               const compgameEmbed = new MessageEmbed()
                 .setColor(setColor)
@@ -65,8 +65,8 @@ async function comp (message, username, user, tag) {
                 .addField('Map', game.metadata.map, true)
                 .addField('Played on', game.metadata.game_start_patched + ' (GMT)', true)
                 .addField('Team & Agent', playerAgentTeam, true)
-                .addField('Red Team', red, true)
-                .addField('Blue Team', blue, true)
+                .addField('Red Team' + teamwin[0], red, true)
+                .addField('Blue Team' + teamwin[1], blue, true)
                 .addField('ScoreBoard [' + game.teams.red.rounds_won + '-' + game.teams.blue.rounds_won + ']', '```' + sb + '```')
               message.channel.send(compgameEmbed)
               return
